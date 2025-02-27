be976de214ee635cb200325204fbeeeb64f0d79e
Hansung Kim <hansung_kim@berkeley.edu>
2025-02-26 16:54:36 -0800

    Turn ElfBackedMem into plain struct

--- src/sim/elf.rs
+++ src/sim/elf.rs
@@ -1,41 +1,16 @@
-use std::path::PathBuf;
-use std::{collections::HashMap, fs, path::Path};
+use std::path::Path;
+use std::{collections::HashMap, fs};
 use std::sync::Arc;
 use goblin::elf::Elf;
-use crate::base::behavior::*;
-use crate::base::component::{component, ComponentBase, IsComponent};
 use crate::base::mem::HasMemory;
 
-#[derive(Default)]
-pub struct ElfBackedMemState {
-    pub sections: HashMap<(usize, usize), Vec<u8>>,
-}
-
-#[derive(Default)]
 pub struct ElfBackedMem {
-    pub base: ComponentBase<ElfBackedMemState, ElfBackedMemConfig>,
-}
-
-#[derive(Default)]
-pub struct ElfBackedMemConfig {
-    pub path: PathBuf,
-}
-
-component!(ElfBackedMem, ElfBackedMemState, ElfBackedMemConfig,
-    fn new(config: Arc<ElfBackedMemConfig>) -> ElfBackedMem {
-        let mut me = ElfBackedMem::default();
-        me.load_path(config.path.as_ref()).expect(&format!("Elf file {:?} not found", config.path));
-        me
-    }
-);
-
-impl ComponentBehaviors for ElfBackedMem {
-    fn tick_one(&mut self) {}
+    pub sections: HashMap<(usize, usize), Vec<u8>>,
 }
 
 impl HasMemory for ElfBackedMem {
     fn read<const N: usize>(&mut self, addr: usize) -> Option<Arc<[u8; N]>> {
-        self.base.state.sections.iter().fold(None, |prev, (range, data)| {
+        self.sections.iter().fold(None, |prev, (range, data)| {
             prev.or(((addr >= range.0) && (addr + N <= range.1)).then(|| {
                 Arc::new(data[(addr - range.0)..(addr - range.0 + N)].try_into().unwrap())
             }))
@@ -48,6 +23,14 @@ impl HasMemory for ElfBackedMem {
 }
 
 impl ElfBackedMem {
+    pub fn new(path: &Path) -> ElfBackedMem {
+        let mut me = ElfBackedMem {
+            sections: Default::default()
+        };
+        me.load_path(path.as_ref()).expect(&format!("Elf file {:?} not found", path));
+        me
+    }
+
     pub fn read_inst(&mut self, addr: usize) -> Option<u64> {
         let slice = self.read::<8>(addr)?;
         u64::from_le_bytes(*slice).into()
@@ -57,7 +40,7 @@ impl ElfBackedMem {
         let data = fs::read(path).map_err(|e| format!("Failed to read file: {}", e))?;
         let elf = Elf::parse(&data).map_err(|e| format!("Failed to parse ELF file: {}", e))?;
 
-        self.base.state.sections = HashMap::new();
+        self.sections = HashMap::new();
 
         // Iterate over the ELF sections
         for section in &elf.section_headers {
@@ -72,7 +55,7 @@ impl ElfBackedMem {
                 let size = section.sh_size as usize;
                 if offset + size <= data.len() {
                     let bytes = data[offset..offset + size].to_vec();
-                    self.base.state.sections.insert(range, bytes);
+                    self.sections.insert(range, bytes);
                 } else {
                     return Err(format!(
                             "Invalid section bounds: offset {} size {}",
--- src/sim/top.rs
+++ src/sim/top.rs
@@ -1,15 +1,13 @@
 use crate::base::behavior::*;
-use crate::base::component::IsComponent;
 use crate::cluster::Cluster;
 use crate::muon::config::MuonConfig;
-use crate::sim::elf::{ElfBackedMem, ElfBackedMemConfig};
+use crate::sim::elf::ElfBackedMem;
 use crate::sim::toy_mem::ToyMemory;
 use std::path::PathBuf;
 use std::sync::{Arc, LazyLock, RwLock};
 
 pub static GMEM: LazyLock<RwLock<ToyMemory>> = LazyLock::new(|| RwLock::new(ToyMemory::default()));
 
-#[derive(Default, Clone)]
 pub struct CyclotronTopConfig {
     pub timeout: u64,
     pub elf_path: PathBuf,
@@ -23,11 +21,7 @@ pub struct CyclotronTop {
 
 impl CyclotronTop {
     pub fn new(config: Arc<CyclotronTopConfig>) -> CyclotronTop {
-        let imem = Arc::new(RwLock::new(ElfBackedMem::new(Arc::new(
-            ElfBackedMemConfig {
-                path: config.elf_path.clone(),
-            },
-        ))));
+        let imem = Arc::new(RwLock::new(ElfBackedMem::new(config.elf_path.as_path())));
 
         let me = CyclotronTop {
             // imem: imem.clone(),
