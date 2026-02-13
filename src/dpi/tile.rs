a5bd21000acb76385af89919d70525f165a68714
Hansung Kim <hansung_kim@berkeley.edu>
2026-02-12 17:04:54 -0800

    tile: Implement stall for dmem response

--- src/dpi/tile.rs
+++ src/dpi/tile.rs
@@ -1,7 +1,8 @@
 use crate::base::behavior::*;
 use crate::dpi::CELL;
 use crate::muon::scheduler::Schedule;
-use crate::muon::warp::Writeback;
+use crate::muon::warp::{ExWriteback, MemRequest, MemResponse, Writeback};
+use std::iter::zip;
 use std::slice::{from_raw_parts, from_raw_parts_mut};
 
 /// Holds the current control state of the Cyclotron pipeline, e.g. what's the
@@ -11,7 +12,8 @@ use std::slice::{from_raw_parts, from_raw_parts_mut};
 pub struct PipelineContext {
     last_warp: usize,
     next_schedule: Option<Schedule>,
-    outstanding_mem: bool,
+    outstanding_mem_req: Option<Vec<Option<MemRequest>>>,
+    staged_mem_resps: Vec<Option<MemResponse>>,
 }
 
 #[no_mangle]
@@ -96,6 +98,9 @@ pub unsafe extern "C" fn cyclotron_tile_tick_rs(
     let neutrino = &mut cluster.neutrino;
     let core = &mut cluster.cores[0];
 
+    let mut ex_writeback: Option<ExWriteback> = None;
+    let mut warp_id = 0usize;
+
     // wait for previous fetch imem req to come back
     if pipe_context.next_schedule.is_some() {
         if imem_resp_valid == 0 {
@@ -114,7 +119,6 @@ pub unsafe extern "C" fn cyclotron_tile_tick_rs(
         let inst: u64 = imem_resp_bits_data;
         let sched = pipe_context.next_schedule.unwrap();
         let uop = core.warps[sched.warp].frontend_nofetch(sched, inst);
-
         let warp = &mut core.warps[sched.warp];
 
         // warp.backend(
@@ -126,56 +130,51 @@ pub unsafe extern "C" fn cyclotron_tile_tick_rs(
         //     .expect("cyclotron: backend() failed");
 
         // collector/EX(alu, fpu)
-        let ex_writeback = warp
-            .backend_beforemem(uop, &mut core.scheduler, neutrino)
-            .expect("cyclotron: execute() failed");
+        ex_writeback = Some(
+            warp.backend_beforemem(uop, &mut core.scheduler, neutrino)
+                .expect("cyclotron: execute() failed"),
+        );
+        warp_id = sched.warp;
 
         // MEM
         // abort if downstream not ready
         let dmem_all_ready = dmem_req_ready.iter().all(|ready| *ready == 1u8);
         if dmem_all_ready {
-            let mem_reqs = ex_writeback.mem_req;
-            for (l, req) in mem_reqs.iter().enumerate() {
+            let mem_req = &ex_writeback.as_ref().unwrap().mem_req;
+            let mut any_lane_valid = false;
+            for (i, req) in mem_req.iter().enumerate() {
                 match req {
                     Some(req) => {
-                        dmem_req_valid[l] = 1u8;
-                        dmem_req_bits_store[l] = 0; // TODO
-                        dmem_req_bits_address[l] = req.addr;
+                        dmem_req_valid[i] = 1u8;
+                        dmem_req_bits_store[i] = 0; // TODO
+                        dmem_req_bits_address[i] = req.addr;
                         // single outstanding req
-                        dmem_req_bits_size[l] = 2; // 32-bit word
-                        dmem_req_bits_tag[l] = 0;
-                        dmem_req_bits_data[l] = 0; // TODO
-                        dmem_req_bits_mask[l] = 0xf; // TODO
-                        // TODO: data, tag
+                        dmem_req_bits_size[i] = 2; // 32-bit word
+                        dmem_req_bits_tag[i] = 0;
+                        dmem_req_bits_data[i] = 0; // TODO
+                        dmem_req_bits_mask[i] = 0xf; // TODO
+                                                     // TODO: data, tag
 
-                        pipe_context.outstanding_mem = true;
-                    },
+                        any_lane_valid = true;
+                    }
                     None => {
-                        dmem_req_valid[l] = 0u8;
-                        dmem_req_bits_store[l] = 0;
-                        dmem_req_bits_address[l] = 0;
-                        dmem_req_bits_size[l] = 2; // 32-bit word
-                        dmem_req_bits_tag[l] = 0;
-                        dmem_req_bits_data[l] = 0;
-                        dmem_req_bits_mask[l] = 0;
+                        dmem_req_valid[i] = 0u8;
+                        dmem_req_bits_store[i] = 0;
+                        dmem_req_bits_address[i] = 0;
+                        dmem_req_bits_size[i] = 2; // 32-bit word
+                        dmem_req_bits_tag[i] = 0;
+                        dmem_req_bits_data[i] = 0;
+                        dmem_req_bits_mask[i] = 0;
                         // TODO: data, tag
                     }
                 }
             }
-
+            if any_lane_valid {
+                pipe_context.outstanding_mem_req = Some(mem_req.clone());
+            }
         } else {
             panic!("dmem is not ready!");
         }
-
-        // TODO: fake writeback
-        let writeback = Writeback {
-            inst: ex_writeback.inst,
-            tmask: ex_writeback.tmask,
-            rd_addr: ex_writeback.rd_addr,
-            rd_data: ex_writeback.rd_data,
-            sched_wb: ex_writeback.sched_wb,
-        };
-        warp.writeback(&writeback);
 
         // TODO: properly stall at MEM; shouldn't replay EX upon mem stall
         //
@@ -184,10 +183,52 @@ pub unsafe extern "C" fn cyclotron_tile_tick_rs(
     }
 
     // MEM: handle response after stalling
-    if pipe_context.outstanding_mem {
-        todo!("work your ass");
+    if let Some(mem_req) = &pipe_context.outstanding_mem_req {
+        // per-lane responses may come back at different times; need to stage them so that the
+        // individual resps don't get lost before all of them comes back
 
-        pipe_context.outstanding_mem = false;
+        for (i, req) in mem_req.iter().enumerate() {
+            if dmem_resp_valid[i] != 1u8 {
+                continue;
+            }
+
+            let req = req.as_ref().expect("dmem response on an invalid req!");
+            assert!(dmem_resp_bits_tag[i] == 0, "dmem req and resp tag mismatch");
+            let data = dmem_resp_bits_data[i].to_le_bytes();
+            let resp = MemResponse {
+                data: (!req.is_store).then_some(data),
+                is_sext: req.is_sext,
+            };
+            // TODO: don't think staged_mem_resps is initialized to the correct length
+            pipe_context.staged_mem_resps[i] = Some(resp);
+        }
+
+        let mut all_reqs_responded = true;
+        for (req, resp) in zip(mem_req, &pipe_context.staged_mem_resps) {
+            let this_req_responded = req.is_none() || resp.is_some();
+            if !this_req_responded {
+                all_reqs_responded = false;
+                break;
+            }
+        }
+        if all_reqs_responded {
+            println!("cyclotron_tile: all lanes dmem req responded!");
+            pipe_context.outstanding_mem_req = None;
+        }
+    }
+
+    // Writeback
+    if let Some(ex_wb) = ex_writeback {
+        // TODO: fake writeback
+        let writeback = Writeback {
+            inst: ex_wb.inst,
+            tmask: ex_wb.tmask,
+            rd_addr: ex_wb.rd_addr,
+            rd_data: ex_wb.rd_data,
+            sched_wb: ex_wb.sched_wb,
+        };
+        let warp = &mut core.warps[warp_id];
+        warp.writeback(&writeback);
     }
 
     // move to fetching the next instruction
